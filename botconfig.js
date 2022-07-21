3ce35712505db4aef23fc2140e6a86c54c36f26e
Samo <86493338+SamoFound@users.noreply.github.com>
2022-07-21 14:58:52 +0200

    Update botconfig.js
    Signed-off-by: Samo <86493338+SamoFound@users.noreply.github.com>

--- botconfig.js
+++ botconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/XRJd6mWdcv", // Support Server Link
-  Token: process.env.Token || "OTg3Nzc0NzQwNjc4NzEzNDE0.GyWnzZ.iyFO_dZogHntfDR52CNyL1l5MQy8g66w-uY0Sw", // Discord Bot Token
+  Token: process.env.Token || process., // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "987774740678713414", // Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
